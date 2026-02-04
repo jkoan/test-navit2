b84925359889253797e66cf06f53cb53d56cf04e
jkoan <jkoan@users.noreply.github.com>
2026-02-04 13:31:53 +0100

    test

--- navit/file.c
+++ navit/file.c
@@ -17,6 +17,7 @@
  * Boston, MA  02110-1301, USA.
  */
 
+
 #include "config.h"
 #include "navit_lfs.h"
 #ifdef _MSC_VER
--- navit/file.h
+++ navit/file.h
@@ -17,6 +17,7 @@
  * Boston, MA  02110-1301, USA.
  */
 
+
 #ifndef NAVIT_FILE_H
 #define NAVIT_FILE_H
 
